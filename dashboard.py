c9cd4b989d426fbe56dfc6654f7395e6a39f9206
LaxmiVatsalyaDaita <87368740+LaxmiVatsalyaDaita@users.noreply.github.com>
2023-11-29 13:07:09 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,9 +15,9 @@ if fl is not None:
     filename = fl.name
     st.write(filename)
     df = pd.read_csv(filename, encoding="ISO-8859-1")
-else: 
-    os.chdir(r"D:\VS Code\sales-dashboard") # change directory
-    df = pd.read_csv(r"Sample-Superstore.csv", encoding="ISO-8859-1")
+#else: 
+#    os.chdir(r"D:\VS Code\sales-dashboard") # change directory
+#    df = pd.read_csv(r"Sample-Superstore.csv", encoding="ISO-8859-1")
 
 
 col1, col2 = st.columns((2))
