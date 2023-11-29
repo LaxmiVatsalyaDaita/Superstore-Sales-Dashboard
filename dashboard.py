0c1c438cf00d2c1e380269d72f0bd87d2a5e4add
LaxmiVatsalyaDaita <87368740+LaxmiVatsalyaDaita@users.noreply.github.com>
2023-11-29 13:09:42 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -18,7 +18,8 @@ if fl is not None:
 #else: 
 #    os.chdir(r"D:\VS Code\sales-dashboard") # change directory
 #    df = pd.read_csv(r"Sample-Superstore.csv", encoding="ISO-8859-1")
-
+else:
+    df = pd.read_csv(r"Sample-Superstore.csv", encoding="ISO-8859-1)
 
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
